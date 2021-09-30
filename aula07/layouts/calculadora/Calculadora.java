2cb573c7dcd1e30a25891f659d31547e25a4fc37
Camila <camila.namour@gmail.com>
2021-09-30 17:47:17 -0300

    aula07-calculadora

--- aula07/layouts/calculadora/Calculadora.java
+++ aula07/layouts/calculadora/Calculadora.java
@@ -29,78 +29,24 @@ public class Calculadora extends Application{
 		layoutTop.setVgap(20.0);
 		layoutTop.setAlignment(Pos.CENTER);
 		layoutTop.getChildren().addAll(txtVisor, btnLimpar);
-		
-		Button btn1 = new Button("1");
-		btn1.setPrefSize(60.0, 70.0);
-		
-		Button btn2 = new Button("2");
-		btn2.setPrefSize(60.0, 70.0);
-		
-		Button btn3 = new Button("3");
-		btn3.setPrefSize(60.0, 70.0);
-		
-		Button btn4 = new Button("4");
-		btn4.setPrefSize(60.0, 70.0);
-		
-		Button btn5 = new Button("5");
-		btn5.setPrefSize(60.0, 70.0);
-		
-		Button btn6 = new Button("6");
-		btn6.setPrefSize(60.0, 70.0);
-		
-		Button btn7 = new Button("7");
-		btn7.setPrefSize(60.0, 70.0);
-		
-		Button btn8 = new Button("8");
-		btn8.setPrefSize(60.0, 70.0);
-		
-		Button btn9 = new Button("9");
-		btn9.setPrefSize(60.0, 70.0);
-		
-		Button btn0 = new Button("0");
-		btn0.setPrefSize(60.0, 70.0);
-		
-		Button btnSomar = new Button("+");
-		btnSomar.setPrefSize(60.0, 70.0);
-		
-		Button btnSubtrair = new Button("-");
-		btnSubtrair.setPrefSize(60.0, 70.0);
-		
-		Button btnMultiplicar = new Button("*");
-		btnMultiplicar.setPrefSize(60.0, 70.0);
-		
-		Button btnDividir = new Button("/");
-		btnDividir.setPrefSize(60.0, 70.0);
-		
-		Button btnPonto = new Button(".");
-		btnPonto.setPrefSize(60.0, 70.0);
-		
-		Button btnIgual = new Button("=");
-		btnIgual.setPrefSize(60.0, 70.0);
-		
+
 		layoutCentro.setAlignment(Pos.CENTER);
 		layoutCentro.setHgap(10.0);
 		layoutCentro.setVgap(10.0);
 		
-		layoutCentro.add(btn1, 0, 0);
-		layoutCentro.add(btn2, 1, 0);
-		layoutCentro.add(btn3, 2, 0);
-		layoutCentro.add(btnSomar, 3, 0);
+//		Button btn1 = new Button("1");
+//		btn1.setPrefSize(60.0, 70.0);
+//		layoutCentro.add(btn1, 0, 0);
 		
-		layoutCentro.add(btn4, 0, 1);
-		layoutCentro.add(btn5, 1, 1);
-		layoutCentro.add(btn6, 2, 1);
-		layoutCentro.add(btnSubtrair, 3, 1);
-		
-		layoutCentro.add(btn7, 0, 2);
-		layoutCentro.add(btn8, 1, 2);
-		layoutCentro.add(btn9, 2, 2);
-		layoutCentro.add(btnMultiplicar, 3, 2);
-		
-		layoutCentro.add(btnPonto, 0, 3);
-		layoutCentro.add(btn0, 1, 3);
-		layoutCentro.add(btnIgual, 2, 3);
-		layoutCentro.add(btnDividir, 3, 3);
+		String[] nomesBotoes = {"1", "2", "3", "+", "4", "5", "6", "-",
+				"7", "8", "9", "*", ".", "0", "=", "/"};
+		for(int i=0; i<nomesBotoes.length; i++){ 
+			int lin=i/4;
+			int col=i%4;
+			Button btn = new Button(nomesBotoes[i]);
+			btn.setPrefSize(60.0, 70.0);
+			layoutCentro.add(btn, col, lin);
+		}
 		
 		layoutPrincipal.setTop(layoutTop);
 		layoutPrincipal.setCenter(layoutCentro);
